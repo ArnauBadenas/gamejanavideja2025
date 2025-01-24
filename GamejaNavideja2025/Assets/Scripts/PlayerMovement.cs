e2164bb01bc70f2cab64f19c86a0b2e73e4101cc
Arnau Badenas <4bvives@gmail.com>
2025-01-24 20:34:33 +0100

    PlayerMovement init

--- GamejaNavideja2025/Assets/Scripts/PlayerMovement.cs
+++ GamejaNavideja2025/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,34 @@
-namespace DefaultNamespace
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement
+    public float speed;
+    public float jumpForce;
+
+    public void Start()
     {
+    }
+
+    public void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    //It moves the player
+    public void Move()
+    {
+        transform.position += new Vector3(InputManager.instance.move.x * speed * Time.deltaTime, 0,
+            InputManager.instance.move.y * speed * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (InputManager.instance.jump.HOLD)
+        {
+            transform.position += new Vector3(0, jumpForce * Time.deltaTime, 0);
+        }
         
     }
 }
