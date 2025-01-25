be09f6ea5d95fc882822c6ec317e9cb207eaa019
Arnau Badenas <4bvives@gmail.com>
2025-01-25 14:06:13 +0100

    Conveyor change speed
    Doesnt work because conveyor speed needs to be independent from each conveyor.

--- GamejaNavideja2025/Assets/Scripts/ConveyorMovement.cs
+++ GamejaNavideja2025/Assets/Scripts/ConveyorMovement.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 public class ConveyorMovement : MonoBehaviour
 {
     public float conveyorMovementSpeed;
-    public Rigidbody rb;
+    //public Rigidbody rb;
+    public float speedIncreaseRate = 0.1f;
+
+    private float timeSinceLastIncrease = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeSinceLastIncrease += Time.deltaTime;
+        if (timeSinceLastIncrease >= speedIncreaseRate)
+        {
+            conveyorMovementSpeed+= speedIncreaseRate;
+            timeSinceLastIncrease = 0f;
+        }
         transform.position += new Vector3(0,0,-conveyorMovementSpeed)*Time.deltaTime;
         //rb.AddForce(new Vector3(0,0,-conveyorMovementSpeed), ForceMode.Force);
         
