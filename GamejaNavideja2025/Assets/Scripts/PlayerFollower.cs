b8489e62674dc967b0b4214245cb37913ce9da71
Arnau Badenas <4bvives@gmail.com>
2025-01-26 11:54:06 +0100

    update levels

---
+++ GamejaNavideja2025/Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, transform.position.y, gameObject.transform.position.z);
+    }
+}
